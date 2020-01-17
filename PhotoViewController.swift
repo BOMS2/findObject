c01f23bb5cb56d69353ff36b4561cbfce65bc1e5
BOMS2 <>
2020-01-18 04:15:24 +0900

    Photo View

--- PhotoViewController.swift
+++ PhotoViewController.swift
@@ -21,43 +21,14 @@ class PhotoViewController: UIViewController, AVCaptureVideoDataOutputSampleBuffe
         super.viewDidLoad()
         
         photoLabel.text = "Waiting..."
-        
-//        let captureSession = AVCaptureSession()
-//        captureSession.sessionPreset = .photo
-//
-//        guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
-//        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
-//        captureSession.addInput(input)
-//
-//        captureSession.startRunning()
-        
-        
-        
-//        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-//        view.layer.addSublayer(previewLayer)
-//        previewLayer.frame = photoView.frame
-//
-//
-//        let dataOutput = AVCaptureVideoDataOutput()
-//        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label:"videoQueue"))
-//        captureSession.addOutput(dataOutput)
-        
-        //        let request = VNCoreMLRequest(model: VNCoreMLModel, completionHandler: VNRequestCompletionHandler?)
-        //        VNImageRequestHandler(cpImage: CGImage, options: [:].perfrom(requests: [VNRequest])
-        
-        
-        
     }
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection){
-        //print("Camera was able to capture a frame: ", Data())
-        
-        
+   
         guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {return}
         
         guard let model = try? VNCoreMLModel(for: Resnet50().model) else {return}
         let request = VNCoreMLRequest(model: model)
         { (finishedReq, err) in
-            // print(finishedReq.results)
             
             guard let results = finishedReq.results as? [VNClassificationObservation]
                 else { return }
